bee3e4c81a5a13b34fd61a49d8fc782dfe2665e3
Jacob Pavlock <jtpavlock@gmail.com>
2020-08-06 21:29:02 -0700

    refactor(cli): refactor cli hook into cli module
    I'm not currently convinced a separate hooks module is needed. I'm currently planning on definining hooks in their relevant modules, and then listing them all together in the documentation.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,9 +8,9 @@ from moe import cli
 class TestArgParse:
     """Test the argument parser."""
 
-    def test_no_args(self):
+    def test_no_args(self, mocker):
         """Test exit if 0 arguments given."""
         with pytest.raises(SystemExit) as pytest_e:
-            cli._parse_args()
+            cli._parse_args([], mocker.Mock())
 
         assert pytest_e.value.code != 0
