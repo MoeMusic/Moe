be060695dd0db0b1186b22f66c4811efff7a173c
Jacob Pavlock <jtpavlock@gmail.com>
2020-08-09 17:56:30 -0700

    refactor(config): Refactor config for better dependency injection
    Makes testing easier.

--- moe/config.py
+++ moe/config.py
@@ -12,19 +12,39 @@ class Config:
 
     Also initializes the database and will be passed to various hooks
     throughout a single run of moe.
+
+    Attributes:
+        config_dir (pathlib.Path): Configuration directory.
+        db_path (pathlib.Path): Path of the database file.
     """
 
-    def __init__(self, config_path: pathlib.Path = None, db_path: pathlib.Path = None):
+    def __init__(
+        self,
+        config_dir: pathlib.Path = pathlib.Path().home() / ".config" / "moe",
+        db_dir: pathlib.Path = None,
+        db_filename: str = "library.db",
+        engine: sqlalchemy.engine.base.Engine = None,
+    ):
         """Read configuration.
 
         Args:
-            config_path: path to configuration file
-            db_path: path to database file
+            config_dir: Path of the configuration directory.
+            db_dir: Path of the database directory. Defaults to config_dir.
+            db_filename: Name of the database file.
+            engine: sqlalchemy database engine to use. Defaults to sqlite
+                located at db_dir / db_filename.
         """
-        self.path = config_path if config_path else pathlib.Path().home() / ".config"
-        self.db_path = db_path if db_path else self.path / "library.db"
+        self.config_dir = config_dir
+        db_dir = db_dir if db_dir else config_dir
+        self.db_path = db_dir / db_filename
+
+        if not self.config_dir.exists():
+            self.config_dir.mkdir(parents=True)
+        if not db_dir.exists():
+            db_dir.mkdir(parents=True)
 
         # initialize db
-        engine = sqlalchemy.create_engine("sqlite:///" + str(self.db_path))
+        if not engine:
+            engine = sqlalchemy.create_engine("sqlite:///" + str(self.db_path))
         library.Session.configure(bind=engine)
         library.Base.metadata.create_all(engine)  # create tables if they don't exist
