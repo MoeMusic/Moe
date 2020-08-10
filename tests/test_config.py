706618fade548119e32000d2467973759cdef620
Jacob Pavlock <jtpavlock@gmail.com>
2020-08-09 17:56:50 -0700

    test(config): Add config tests

---
+++ tests/test_config.py
@@ -0,0 +1,17 @@
+"""Test configuration."""
+
+import moe.config
+
+
+class TestInit:
+    """Test configuration initialization."""
+
+    def test_dirs_dne(self, tmp_path, mocker):
+        """Should create the config and database directories if they don't exist."""
+        fake_path = tmp_path / "this_definitely_doesnt_exist"
+        fake_path2 = tmp_path / "this_definitely_doesnt_exist2"
+
+        moe.config.Config(config_dir=fake_path, db_dir=fake_path2, engine=mocker.Mock())
+
+        assert fake_path.exists()
+        assert fake_path2.exists()
