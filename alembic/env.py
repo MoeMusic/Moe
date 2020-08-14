f9687933338a30c55c63974513107d524d1cc163
Jacob Pavlock <jtpavlock@gmail.com>
2020-08-13 19:32:35 -0700

    feat(alembic): use alembic for database migrations

---
+++ alembic/env.py
@@ -0,0 +1,60 @@
+"""This is a Python script that is run whenever the alembic migration tool is invoked.
+
+At the very least, it contains instructions to configure and generate a SQLAlchemy
+engine, procure a connection from that engine along with a transaction, and then
+invoke the migration engine, using the connection as a source of database connectivity.
+
+The env.py script is part of the generated environment so that the way migrations run
+is entirely customizable. The exact specifics of how to connect are here, as well as
+the specifics of how the migration environment are invoked. The script can be modified
+so that multiple engines can be operated upon, custom arguments can be passed into the
+migration environment, application-specific libraries and models can be loaded in
+and made available.
+
+Alembic includes a set of initialization templates which feature different varieties
+of env.py for different use cases.
+"""
+
+from logging.config import fileConfig
+
+from alembic import context
+from moe.core import library
+from moe.core.config import Config
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+fileConfig(config.config_file_name)
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
+target_metadata = library.Base.metadata
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
+
+def run_migrations_online():
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine and associate a connection
+    with the context.
+    """
+    config = Config()
+    engine = config.engine
+
+    with engine.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata)
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+run_migrations_online()
