8302978ad46fd61ef943c014e9d8cf107df0cc0c
Jacob Pavlock <jtpavlock@gmail.com>
2020-08-24 18:47:19 -0700

    refactor(config): simplify config

--- tests/test_config.py
+++ tests/test_config.py
@@ -12,10 +12,8 @@ class TestInit:
     def test_dirs_dne(self, tmp_path):
         """Should create the config and database directories if they don't exist."""
         fake_path = tmp_path / "this_definitely_doesnt_exist"
-        fake_path2 = tmp_path / "this_definitely_doesnt_exist2"
 
         with patch.object(Config, "_db_init"):
-            Config(config_dir=fake_path, db_dir=fake_path2)
+            Config(config_dir=fake_path)
 
         assert fake_path.exists()
-        assert fake_path2.exists()
