c73f898bd56414414e3e3059767c2550e21159ba
Jacob Pavlock <jtpavlock@gmail.com>
2020-09-07 22:09:54 -0500

    feat(add): add ability to add directories (albums)

--- tests/test_add.py
+++ tests/test_add.py
@@ -15,19 +15,8 @@ from moe.plugins import add
 class TestParseArgs:
     """Test the plugin argument parser."""
 
-    def test_track(self, tmp_session):
-        """Tracks are added to the database."""
-        music_file = pathlib.Path("tests/resources/audio_files/full.mp3")
-        args = argparse.Namespace(path=str(music_file))
-
-        add.parse_args(Mock(), tmp_session, args)
-
-        test_path = tmp_session.query(Track.path).scalar()
-
-        assert test_path == music_file.resolve()
-
-    def test_file_not_found(self, tmp_session):
-        """We should raise SystemExit if we can't find the file to add."""
+    def test_path_not_found(self):
+        """Raise SystemExit if the path to add does not exist."""
         args = argparse.Namespace(path="does not exist")
 
         with pytest.raises(SystemExit) as error:
@@ -35,24 +24,59 @@ class TestParseArgs:
 
         assert error.value.code != 0
 
-    def test_duplicate_file(self, tmp_session):
-        """We should raise SystemExit if the file already exists in the library."""
-        args = argparse.Namespace(path="tests/resources/audio_files/full.mp3")
 
-        add.parse_args(Mock(), tmp_session, args)
+class TestAddTrack:
+    """Test adding a track."""
+
+    def test_track(self, tmp_session):
+        """Tracks are added to the database."""
+        music_file = pathlib.Path("tests/resources/audio_files/full.mp3")
+
+        add.add_track(music_file)
+
+        test_path = tmp_session.query(Track.path).scalar()
+
+        assert test_path == music_file.resolve()
+
+    def test_bad_music_file(self, tmp_path):
+        """Raise SystemExit if the file given is not a valid music file."""
+        bad_file = tmp_path / "a"
+        bad_file.touch()
 
         with pytest.raises(SystemExit) as error:
-            add.parse_args(Mock(), tmp_session, args)
+            add.add_track(bad_file)
 
         assert error.value.code != 0
+
+    def test_duplicate_file(self, tmp_session):
+        """Raise SystemExit if the file already exists in the library."""
+        music_file = pathlib.Path("tests/resources/audio_files/full.mp3")
+        add.add_track(music_file)
+
+        with pytest.raises(SystemExit) as error:
+            add.add_track(music_file)
+
+        assert error.value.code != 0
+
+
+class TestAddAlbum:
+    """Test adding an album."""
+
+    def test_album(self, tmp_session):
+        """Test we can add all tracks in a directory."""
+        album_path = pathlib.Path("tests/resources/album")
+        add.add_album(album_path)
+
+        for track in album_path.rglob("*.mp3"):
+            assert tmp_session.query(Track.path).filter_by(path=track.resolve())
 
 
 @pytest.mark.integration
 class TestCommand:
     """Test cli integration with the add command."""
 
-    def test_basic(self, tmp_config):
-        """Music is added to the library when the `add` command is invoked."""
+    def test_file(self, tmp_config):
+        """Tracks are added to the library when a file is passed to `add`."""
         args = ["moe", "add", "tests/resources/audio_files/full.mp3"]
 
         with patch("sys.argv", args):
@@ -61,3 +85,16 @@ class TestCommand:
 
         with session_scope() as session:
             assert session.query(Track._id).scalar()
+
+    def test_dir(self, tmp_config):
+        """Directories are recursively searched for tracks when passed to `add`."""
+        album_path = "tests/resources/album"
+        args = ["moe", "add", album_path]
+
+        with patch("sys.argv", args):
+            with patch("moe.cli.Config", return_value=tmp_config):
+                cli.main()
+
+        with session_scope() as session:
+            for track in pathlib.Path(album_path).rglob("*.mp3"):
+                assert session.query(Track.path).filter_by(path=track.resolve())
