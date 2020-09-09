15bd6e1f14bd03951041a0dee11b87b02f52448a
Jacob Pavlock <jtpavlock@gmail.com>
2020-09-08 22:19:46 -0500

    feat(add): add now accepts multiple path arguments

--- moe/plugins/add.py
+++ moe/plugins/add.py
@@ -21,7 +21,9 @@ def addcommand(cmd_parsers: argparse._SubParsersAction):  # noqa: WPS437
     add_parser = cmd_parsers.add_parser(
         "add", description="Adds music to the library.", help="add music to the library"
     )
-    add_parser.add_argument("path", help="path to the music you want to add")
+    add_parser.add_argument(
+        "paths", nargs="+", help="dir to add an album or file to add a track"
+    )
     add_parser.set_defaults(func=parse_args)
 
 
@@ -36,16 +38,31 @@ def parse_args(config: Config, session: Session, args: argparse.Namespace):
     Raises:
         SystemExit: Path given does not exist.
     """
-    path = pathlib.Path(args.path)
+    paths = [pathlib.Path(arg_path) for arg_path in args.paths]
 
-    if not path.exists():
-        log.error(f"Path not found: {path}")
-        raise SystemExit(1)
+    for path in paths:
+        if not path.exists():
+            log.error(f"Path not found: {path}")
+            raise SystemExit(1)
 
+        _add_path(path)
+
+
+def _add_path(path: pathlib.Path):
+    """Add a given path to the library.
+
+    Args:
+        path: Path of the music to add. If a file, add as a track,
+            if a directory, add as an album.
+
+    Raises:
+        SystemExit: Unable to add the given path to the library.
+    """
     if path.is_file():
         try:
             _add_track(path)
         except (TypeError, mediafile.UnreadableFileError, DbDupTrackError):
+            log.error(f"Unable to add track: {path}")
             raise SystemExit(1)
 
 
