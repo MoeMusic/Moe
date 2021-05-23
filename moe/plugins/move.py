e6a12554fa2751653ca917772454af18bda260dd
Jacob Pavlock <jtpavlock@gmail.com>
2021-05-22 19:27:05 -0700

    feat(move): add move plugin
    Adds the ability to move library items after they are added.

---
+++ moe/plugins/move.py
@@ -0,0 +1,81 @@
+"""Any operations regarding altering the location of files in the library."""
+
+import logging
+import pathlib
+import shutil
+
+import dynaconf
+
+import moe
+from moe.core.config import Config
+from moe.core.library.album import Album
+from moe.core.library.music_item import MusicItem
+from moe.core.library.track import Track
+
+log = logging.getLogger(__name__)
+
+
+class MoveError(Exception):
+    """Error moving an item."""
+
+
+# TODO: add file extension to track and include in path format
+@moe.hookimpl
+def add_config_validator(settings: dynaconf.base.LazySettings):
+    """Validate move plugin configuration settings."""
+    settings.validators.register(
+        dynaconf.Validator("LIBRARY_PATH", must_exist=True, default="~/Music")
+    )
+
+
+@moe.hookimpl
+def post_add(config: Config, item: MusicItem):
+    """Copies `item` to the library_path specified in the config.
+
+    Also will format the path of the item's copied according to `track_path_fmt`.
+    This hook is run after an item is added to the music library.
+
+    Args:
+        config: moe config
+        item: Item to be copied.
+    """
+    root_dest = pathlib.Path(config.settings.library_path).expanduser()
+    if isinstance(item, Track):
+        _move_track(item, root_dest)
+    elif isinstance(item, Album):
+        _move_album(item, root_dest)
+
+
+def _move_track(track: Track, root: pathlib.Path):
+    """Copies and formats the destination of a single track.
+
+    The track will overwrite anything that currently exists at the destination path.
+
+    Args:
+        track: track to copy
+        root: root folder to copy the track to
+    """
+    track_path_fmt = (
+        f"{track.albumartist}/{track.album} ({track.year})/"
+        f"{track.track_num} - {track.title}"
+    )
+    track_dest = root / track_path_fmt
+
+    if track_dest.is_file():
+        track_dest.unlink()
+    track_dest.parents[0].mkdir(parents=True, exist_ok=True)
+    shutil.copyfile(track.path, track_dest)  # type: ignore
+
+    track.path = track_dest
+    track.add_to_db()
+
+
+def _move_album(album: Album, root: pathlib.Path):
+    """Copies and formats the destination of an album.
+
+    Args:
+        album: Album to copy.
+        root: Root folder to copy the album to.
+    """
+    for track in album.tracks:
+        _move_track(track, root)
---
+++ tests/test_move.py
@@ -0,0 +1,123 @@
+"""Test the move plugin."""
+
+import pathlib
+from unittest.mock import patch
+
+import pytest
+
+from moe import cli
+from moe.core.library.session import session_scope
+from moe.core.library.track import Album, Track
+from moe.plugins import move
+
+
+class TestPostAdd:
+    """Test functionality of the post-add hook."""
+
+    def test_copy_track(self, real_track, tmp_config, tmp_path, tmp_session):
+        """Test we can copy a Track that was added to the library.
+
+        The track's path should refer to the destination.
+        """
+        config = tmp_config(f'library_path = "{tmp_path.resolve()}"')
+        origin_track_path = real_track.path
+
+        move.post_add(config=config, item=real_track)
+
+        assert tmp_path in real_track.path.parents  # accounts for track path formatting
+        assert origin_track_path.is_file()
+        assert real_track.path.is_file()
+
+    def test_copy_album(self, real_album, tmp_config, tmp_path, tmp_session):
+        """Test we can copy an Album that was added to the library.
+
+        Copying an album is just copying each item belonging to that album.
+        """
+        config = tmp_config(f'library_path = "{tmp_path.resolve()}"')
+        for track in real_album.tracks:
+            origin_track_paths = []
+            origin_track_paths.append(track.path)
+
+        move.post_add(config=config, item=real_album)
+
+        for copied_track in real_album.tracks:
+            assert tmp_path in copied_track.path.parents
+            assert copied_track.path.is_file()
+
+        for origin_track_path in origin_track_paths:
+            assert origin_track_path.is_file()
+
+    def test_home_dir(self, mock_track, tmp_config, tmp_path, tmp_session):
+        """Home directories are allowed to be shortened with '~' in the config."""
+        config = tmp_config('library_path = "~"')
+
+        move.post_add(config=config, item=mock_track)
+
+        assert pathlib.Path.home() in mock_track.path.parents
+
+    def test_path_updated_in_db(self, real_track, tmp_config, tmp_path, tmp_session):
+        """Make sure the path updates are being reflected in the DB."""
+        config = tmp_config(f'library_path = "{tmp_path.resolve()}"')
+        move.post_add(config=config, item=real_track)
+
+        db_track = tmp_session.query(Track).one()
+        assert db_track.path == real_track.path
+
+    def test_duplicate(self, real_track_factory, tmp_config, tmp_path, tmp_session):
+        """Overwrite duplicate tracks that already exist in the db."""
+        track1 = real_track_factory()
+        track2 = real_track_factory()
+        track1.genre = ["rap"]
+        track2.genre = ["hip hop"]
+        track2.track_num = track1.track_num
+        config = tmp_config(f'library_path = "{tmp_path.resolve()}"')
+
+        move.post_add(config=config, item=track1)
+        move.post_add(config=config, item=track2)
+
+        db_track = tmp_session.query(Track).one()
+        assert db_track.genre == track2.genre
+
+
+@pytest.mark.integration
+class TestAddEntry:
+    """Test integration with the `add` command entry of `move`."""
+
+    def test_add_track(self, tmp_config, tmp_path):
+        """Tracks are copied to `library_path` after they are added."""
+        args = ["moe", "add", "tests/resources/audio_files/full.mp3"]
+
+        config = tmp_config(
+            settings=f"""
+                library_path = "{tmp_path}"
+                default_plugins = ["add", "move"]
+                """
+        )
+        with patch("sys.argv", args):
+            with patch("moe.cli.Config", return_value=config):
+                cli.main()
+
+        with session_scope() as session:
+            track = session.query(Track).one()
+            assert tmp_path in track.path.parents  # accounts for track path formatting
+
+    def test_add_album(self, tmp_config, tmp_path):
+        """Albums are copied to `library_path` after they are added."""
+        args = ["moe", "add", "tests/resources/album/"]
+
+        config = tmp_config(
+            settings=f"""
+                library_path = "{tmp_path}"
+                default_plugins = ["add", "move"]
+                """
+        )
+        with patch("sys.argv", args):
+            with patch("moe.cli.Config", return_value=config):
+                cli.main()
+
+        with session_scope() as session:
+            album = session.query(Album).one()
+            for track in album.tracks:
+                assert (
+                    tmp_path in track.path.parents
+                )  # accounts for track path formatting
