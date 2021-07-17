2e395aa99971fc75b0fc47da0d96656c8263b9a7
Jacob Pavlock <jtpavlock@gmail.com>
2021-07-17 08:37:37 -0700

    docs(theme): switch to furo theme

--- docs/conf.py
+++ docs/conf.py
@@ -49,9 +49,8 @@ rst_prolog = open("global.rst", "r").read()  # noqa: WPS515
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = "alabaster"
+# The theme to use for HTML and HTML Help pages.
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
