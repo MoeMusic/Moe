d3b9a3b2dbd4860e8e03fbf1a992b7934e0a10ef
Jacob Pavlock <jtpavlock@gmail.com>
2022-09-08 13:03:27 -0700

    refactor(add): use the `remove` api to remove duplicate items
    All library removals should be done through the `remove` api.

--- tests/plugins/add/test_add_cli.py
+++ tests/plugins/add/test_add_cli.py
@@ -22,67 +22,18 @@ def mock_add() -> Iterator[FunctionType]:
         yield mock_add
 
 
+class TestConfiguration:
+    """Test plugin configuration validation."""
+
+    def test_remove_plugin_exists(self, tmp_config):
+        """The 'add cli' plugin relies on the 'remove' plugin so it must exist."""
+        tmp_config(settings="default_plugins = ['add', 'remove']")
+
+
 @pytest.fixture
 def tmp_add_config(tmp_config) -> Config:
     """A temporary config for the add plugin with the cli."""
-    return tmp_config('default_plugins = ["cli", "add"]')
-
-
-class TestCommand:
-    """Test the `add` command."""
-
-    def test_item(self, mock_add, tmp_add_config):
-        """Items are added to the library when given a path."""
-        mock_path = Path("")
-        cli_args = ["add", str(mock_path)]
-
-        moe.cli.main(cli_args, tmp_add_config)
-
-        mock_add.assert_called_once_with(tmp_add_config, mock_path)
-
-    def test_multiple_items(self, mock_add, tmp_add_config):
-        """Items are added to the library when given a path."""
-        mock_path1 = Path("1")
-        mock_path2 = Path("2")
-        cli_args = ["add", str(mock_path1), str(mock_path2)]
-
-        moe.cli.main(cli_args, tmp_add_config)
-
-        mock_add.assert_any_call(tmp_add_config, mock_path1)
-        mock_add.assert_any_call(tmp_add_config, mock_path2)
-        assert mock_add.call_count == 2
-
-    def test_single_error(self, mock_add, tmp_add_config):
-        """Don't exit after the first failed item if more to be added.
-
-        Still exit with non-zero code if any failures occured.
-        """
-        mock_path1 = Path("1")
-        mock_path2 = Path("2")
-        cli_args = ["add", str(mock_path1), str(mock_path2)]
-
-        mock_add.side_effect = [add.AddError, None]
-        with pytest.raises(SystemExit) as error:
-            moe.cli.main(cli_args, tmp_add_config)
-
-        assert error.value.code != 0
-        mock_add.assert_any_call(tmp_add_config, mock_path2)
-
-
-class TestPluginRegistration:
-    """Test the `plugin_registration` hook implementation."""
-
-    def test_no_cli(self, tmp_config):
-        """Don't enable the add cli plugin if the `cli` plugin is not enabled."""
-        config = tmp_config(settings='default_plugins = ["add"]')
-
-        assert not config.plugin_manager.has_plugin("add_cli")
-
-    def test_cli(self, tmp_config):
-        """Enable the add cli plugin if the `cli` plugin is enabled."""
-        config = tmp_config(settings='default_plugins = ["add", "cli"]')
-
-        assert config.plugin_manager.has_plugin("add_cli")
+    return tmp_config('default_plugins = ["cli", "add", "remove"]')
 
 
 class TestPreAdd:
@@ -263,3 +214,81 @@ class TestPreAdd:
             )
 
             mock_choice_prompt.assert_not_called()
+
+
+class TestCommand:
+    """Test the `add` command."""
+
+    def test_item(self, mock_add, tmp_add_config):
+        """Items are added to the library when given a path."""
+        mock_path = Path("")
+        cli_args = ["add", str(mock_path)]
+
+        moe.cli.main(cli_args, tmp_add_config)
+
+        mock_add.assert_called_once_with(tmp_add_config, mock_path)
+
+    def test_multiple_items(self, mock_add, tmp_add_config):
+        """Items are added to the library when given a path."""
+        mock_path1 = Path("1")
+        mock_path2 = Path("2")
+        cli_args = ["add", str(mock_path1), str(mock_path2)]
+
+        moe.cli.main(cli_args, tmp_add_config)
+
+        mock_add.assert_any_call(tmp_add_config, mock_path1)
+        mock_add.assert_any_call(tmp_add_config, mock_path2)
+        assert mock_add.call_count == 2
+
+    def test_single_error(self, mock_add, tmp_add_config):
+        """Don't exit after the first failed item if more to be added.
+
+        Still exit with non-zero code if any failures occured.
+        """
+        mock_path1 = Path("1")
+        mock_path2 = Path("2")
+        cli_args = ["add", str(mock_path1), str(mock_path2)]
+
+        mock_add.side_effect = [add.AddError, None]
+        with pytest.raises(SystemExit) as error:
+            moe.cli.main(cli_args, tmp_add_config)
+
+        assert error.value.code != 0
+        mock_add.assert_any_call(tmp_add_config, mock_path2)
+
+
+class TestPluginRegistration:
+    """Test the `plugin_registration` hook implementation."""
+
+    def test_no_cli(self, tmp_config):
+        """Don't enable the add cli plugin if the `cli` plugin is not enabled."""
+        config = tmp_config(settings='default_plugins = ["add"]')
+
+        assert not config.plugin_manager.has_plugin("add_cli")
+
+    def test_cli(self, caplog, tmp_config):
+        """Enable the add cli plugin if the `cli` plugin is enabled."""
+        config = tmp_config(settings='default_plugins = ["add", "cli"]')
+
+        assert config.plugin_manager.has_plugin("add_cli")
+
+    def test_cli_no_remove(self, caplog, tmp_config):
+        """Log a warning if the remove plugin is not enabled.
+
+        The 'add cli' plugin depends on the remove plugin for duplicate resolution.
+        """
+        tmp_config(settings='default_plugins = ["add", "cli"]')
+
+        assert any(record.levelname == "WARNING" for record in caplog.records)
+
+    def test_cli_remove(self, caplog, tmp_config):
+        """Don't log a warning if the remove plugin is enabled."""
+        tmp_config(settings='default_plugins = ["add", "cli", "remove"]')
+
+        assert not any(record.levelname == "WARNING" for record in caplog.records)
+
+    def test_no_cli_no_remove(self, caplog, tmp_config):
+        """Only require the remove plugin for the cli."""
+        tmp_config(settings='default_plugins = ["add"]')
+
+        assert not any(record.levelname == "WARNING" for record in caplog.records)
