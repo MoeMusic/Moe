a907dd42ef01d8ab23b47ff0c5462973297c0d26
Jacob Pavlock <jtpavlock@gmail.com>
2022-10-02 10:28:42 -0700

    fix: import debug statements

--- moe/plugins/moe_import/import_cli.py
+++ moe/plugins/moe_import/import_cli.py
@@ -105,7 +105,7 @@ def import_prompt(
     Raises:
         AbortImport: Import prompt was aborted by the user.
     """
-    log.debug("Running import prompt. [{old_album=!r}, {new_album=!r}]")
+    log.debug(f"Running import prompt. [{old_album=!r}, {new_album=!r}]")
 
     console.print(_fmt_import_updates(old_album, new_album))
 
