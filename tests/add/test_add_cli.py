97a46a4c4f37b42f96fa4abe89d4b3f1a8c05d8a
Jacob Pavlock <jtpavlock@gmail.com>
2025-06-12 13:47:39 +0200

    test: remove unused caplog arg

--- tests/add/test_add_cli.py
+++ tests/add/test_add_cli.py
@@ -190,7 +190,7 @@ class TestPluginRegistration:
 
         assert not config.pm.has_plugin("add_cli")
 
-    def test_cli(self, caplog, tmp_config):
+    def test_cli(self, tmp_config):
         """Enable the add cli plugin if the `cli` plugin is enabled."""
         config = tmp_config(settings='default_plugins = ["add", "cli"]')
 
